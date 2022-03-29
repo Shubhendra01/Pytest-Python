b8502a7be99693ddf0c4f2a2c45830b7cc0d00de
unknown <shubhhhh17@gmail.com>
2022-03-29 15:38:15 +0530

    Calculator test cases using mark Parametrize is done

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,22 +1,22 @@
+import pytest
 import Calculator
 
-def test_add():
-    x = 10
-    y = 25
-    result = Calculator.add(x, y)
-    assert x+y==result
-def test_sub():
-    x = 10
-    y = 25
-    result = Calculator.sub(x, y)
-    assert x-y==result
-def test_mul():
-    x = 10
-    y = 25
-    result = Calculator.mul(x, y)
-    assert x*y==result
-def test_div():
-    x = 10
-    y = 25
-    result = Calculator.div(x, y)
-    assert x/y==result
+@pytest.mark.parametrize("a,b,c", [(3,2,5),(10,12,15),(2,5,8),(7,8,15)])
+def test_add(a,b,c):
+    result = Calculator.add(a, b)
+    assert c==result
+
+@pytest.mark.parametrize("a,b,c", [(7,2,5),(12,10,2),(12,5,7),(8,6,2)])
+def test_sub(a,b,c):
+    result = Calculator.sub(a, b)
+    assert c==result
+
+@pytest.mark.parametrize("a,b,c", [(1,5,5),(10,12,120),(2,5,10),(7,8,56)])
+def test_mul(a,b,c):
+    result = Calculator.mul(a, b)
+    assert c==result
+
+@pytest.mark.parametrize("a,b,c", [(4,2,2),(100,4,25),(2,5,8),(7,8,15)])
+def test_div(a,b,c):
+    result = Calculator.div(a, b)
+    assert c==result
